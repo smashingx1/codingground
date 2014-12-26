3cff8a11607456e9e80f4556f34838a927e3e868
vforsythe <smashingx@gmail.com>
2014-12-26 13:08:11 -0700

    Created by Coding Ground Client

---
+++ phpproject/pagelist.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Defs for build script
+ */
+$chunks = array('register', 'register2', 'cc-link', 'home', 'login', 'profile', 'find-business', 'f-and-b',
+    'get-ride', 'ride-detail', 'sponsors', 'sponsors-detail', 'general-information', 'general-info-detail',
+    'account-information', 'redeem-1', 'redeem-2', 'history', 'gamification', 'contest-detail', 'featured-offer',
+    'more-featured', 'drinks-of-the-month', 'filter', 'rewards_by_page_subpage', 'rewards_by_page_ride_partners_subpage', 'general-info-contact');
